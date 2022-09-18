fde3438acad9e4fdd25f040fbf63128b3a56cf1f
Stella <stellanacleto@gmail.com>
2022-09-18 15:51:29 -0300

    Atualização de ClienteController

--- src/main/java/gui/ClienteController.java
+++ src/main/java/gui/ClienteController.java
@@ -1,18 +1,24 @@
 package gui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import models.Cliente;
-import models.Login;
-import models.Usuario;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import models.*;
 import negocio.ServidorAcademia;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.Duration;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ClienteController implements Initializable {
@@ -31,9 +37,26 @@ public class ClienteController implements Initializable {
     private Label lblAltura;
     @FXML
     private Label lblGenero;
+    @FXML
+    private TableView<Exercicio> tableViewExercicios;
+    @FXML
+    private TableView<Treino> tableViewTreinos;
+    @FXML
+    private TableColumn<Treino, String> columnTreinosTipo;
+    @FXML
+    private TableColumn<Exercicio, String> columnExerciciosNome;
+    @FXML
+    private TableColumn<Exercicio, String> columnExerciciosTipo;
+    @FXML
+    private TableColumn<Exercicio, Duration> columnExerciciosIntervalo;
+    @FXML
+    private TableColumn<Exercicio, Integer> columnExerciciosSeries;
+    @FXML
+    private TableColumn<Exercicio, Integer> columnExerciciosRepeticoes;
 
-    //ServidorAcademia servidor = ServidorAcademia.getInstance();
-    //ServidorAcademia.
+    private List<Exercicio> listExercicios = new ArrayList();
+
+    private ObservableList<Exercicio> observableListExercicios;
     Login login = new Login("maria", "123");
     Usuario cliente = new Cliente("32", "Maria Beatriz", "F", login, LocalDate.of(2000, 2,14),57,1.57);
 
@@ -47,6 +70,28 @@ public class ClienteController implements Initializable {
         lblGenero.setText(String.valueOf((((Cliente) cliente).getGenero())));
         lblDtNascimento.setText(String.valueOf(cliente.getDtNascimento()));
 
+
+    }
+
+    public void carregarTableViewClientes() {
+
+        columnExerciciosNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
+        columnExerciciosTipo.setCellValueFactory(new PropertyValueFactory<>("tipo"));
+        columnExerciciosIntervalo.setCellValueFactory(new PropertyValueFactory<>("intervalo"));
+        columnExerciciosSeries.setCellValueFactory(new PropertyValueFactory<>("series"));
+        columnExerciciosRepeticoes.setCellValueFactory(new PropertyValueFactory<>("repeticoes"));
+
+        Exercicio exer1 = new Exercicio("Supino Reto", "Peito", Duration.ofMinutes(10), 4, 10);
+        Exercicio exer2 = new Exercicio("Puxada Aberta", "Costas", Duration.ofMinutes(15), 4, 10);
+        Exercicio exer3 = new Exercicio("Tríceps Pulley", "Tríceps", Duration.ofMinutes(7), 3, 12);
+
+        listExercicios.add(exer1);
+        listExercicios.add(exer2);
+        listExercicios.add(exer3);
+
+        observableListExercicios = FXCollections.observableArrayList(listExercicios);
+
+        tableViewExercicios.setItems(observableListExercicios);
     }
 
     public void userLogOut(ActionEvent event) throws IOException {
