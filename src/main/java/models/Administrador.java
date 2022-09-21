e5f9fbaa8b51044913fa50c164834304cb27a5a3
Stella <stellanacleto@gmail.com>
2022-09-21 10:20:41 -0300

    Atualização de pagamento.fxml e aplicação de serial em Administrador

--- src/main/java/models/Administrador.java
+++ src/main/java/models/Administrador.java
@@ -1,8 +1,11 @@
 package models;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
-public class Administrador extends Usuario{
+public class Administrador extends Usuario implements Serializable {
+
+    private static final long serialVersionUID = 4864028016396120834L;
 
     public Administrador(String id, String nome, String email, String senha, LocalDate dtNascimento) {
         super(id, nome, email, senha, dtNascimento);
